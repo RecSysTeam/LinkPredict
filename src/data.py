bb201a3052b17237c613bf31d1ba3fa5a006208e
Ivan Gerasimov <ivan.gerasimov@mdinc.ru>
2023-03-23 19:27:54 +0300

    install pre-commit

--- src/data.py
+++ src/data.py
@@ -1,21 +1,18 @@
 from pathlib import Path
 from typing import Union
+
 import obsidiantools.api as otools
 
-
 ATTACHMENTS = True
+
 
 def extract_vault(path_voult_folder: Union[Path, str]):
     path_voult_folder = Path(path_voult_folder)
 
-    assert path_voult_folder.exists(), f'Directory {path_voult_folder} does not exsist'
+    assert path_voult_folder.exists(), f"Directory {path_voult_folder} does not exsist"
 
-    vault = (otools.Vault(path_voult_folder)
-        .connect(attachments=ATTACHMENTS)
-        .gather()
-        )
+    vault = otools.Vault(path_voult_folder).connect(attachments=ATTACHMENTS).gather()
     df = vault.get_all_file_metadata()
     print(df.info())
 
     return vault
-    
