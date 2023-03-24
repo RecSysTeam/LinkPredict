27179338f4ee742390ce1a98e03620d510b331ed
avzelentsov <avzelentsov@avito.ru>
2023-03-24 05:29:05 +0300

    add bert_lightfm exp

--- evaluation.py
+++ evaluation.py
@@ -5,6 +5,19 @@ def topn_recommendations(scores, topn=10):
     recommendations = np.apply_along_axis(topidx, 1, scores, topn)
     return recommendations
 
+def downvote_seen_items(scores, data, data_description):
+    assert isinstance(scores, np.ndarray), 'Scores must be a dense numpy array!'
+    itemid = data_description['items']
+    userid = data_description['users']
+    # get indices of observed data, corresponding to scores array
+    # we need to provide correct mapping of rows in scores array into
+    # the corresponding user index (which is assumed to be sorted)
+    row_idx, test_users = pd.factorize(data[userid], sort=True)
+    assert len(test_users) == scores.shape[0]
+    col_idx = data[itemid].values
+    # downvote scores at the corresponding positions
+    scores[row_idx, col_idx] = scores.min() - 1
+
 
 def topidx(a, topn):
     parted = np.argpartition(a, -topn)[-topn:]
